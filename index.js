8d9492c397fb386be59b4ec8b64823d745c0df21
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-12 15:26:12 -0400

    User inputs. Modified code to take inputs and not hardcode

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const fs = require('fs');
-const { circle, rectangle, triangle } = require('./lib/shapes');
+const inquirer = require('inquirer');
+const { Circle, Rectangle, Triangle } = require('./lib/shapes');
 
-const filename = `circle.svg`;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Pick up to 3 characters for your logo',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a text color (OR a hexadecimal number)',
+      name: 'textColor',
+    },
+    {
+      type: 'list',
+      message: 'Pick a shape',
+      name: 'shape',
+      choices: ['circle', 'rectangle', 'triangle'],
+    },
+    {
+      type: 'input',
+      message: 'Please enter a shape color (OR a hexadecimal number)',
+      name: 'shapeColor',
+    },
+  ])
+  .then((data) => {
+    const filename = `logo.svg`;
+    let logo;
 
-fs.writeFile(filename, triangle, (err) =>
-    err ? console.log(err) : console.log('Success!')
-);
+    switch (data.shape) {
+        case "circle":
+            logo = new Circle(data.shapeColor, data.textColor, data.text);
+        case "rectangle":
+            logo = new Rectangle(data.shapeColor, data.textColor, data.text);
+        case "triangle":
+            logo = new Triangle(data.shapeColor, data.textColor, data.text);
+    }
+
+    fs.writeFile(filename, logo.render(), (err) =>
+        err ? console.log(err) : console.log('Generated logo.svg')
+    );
+  });
